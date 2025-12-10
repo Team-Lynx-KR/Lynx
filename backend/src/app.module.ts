e6350c3da3a24e618b2515b9a78aca12fc1ee839
성인석 <inseok1999@gmail.com>
2025-12-11 01:48:37 +0900

    chore: app.module.ts 수정 및 불필요한 파일 삭제

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -27,7 +25,5 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
